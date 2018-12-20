f7e292e893e50ca0badd03f25e5a9407f741a6b6
NicolasFlandrois <n.flandrois9github@gmail.com>
2018-12-20 15:54:35 +0100

    Created Xmas Countdown & first tests > xmascountdown.py

---
+++ xmascountdown.py
@@ -0,0 +1,41 @@
+#! usr/bin/python3
+
+# Thu 20 Dec 2018 03:04:22 PM CET 
+# Author: Nicolas Flandrois
+# Description: Christmas countdown, according to a choosen year.
+# NB: This script is not UTC/Timezone aware. Time is set as naive, using your local (computer) timezone as default.
+
+import time
+import datetime
+import os
+import platform
+import sys
+
+def clean():
+	if platform.system() == "Windows":
+		os.system("cls")
+	elif platform.system() == "Linux":
+		os.system("clear")
+
+try:
+    Year = int(input("Xmas of which year? "))
+except:
+    print("Oops! ",sys.exc_info()[0],""" occured. That was no valid number.  Try again...
+choose a year, in number, format YYYY.""")
+
+xd = datetime.datetime(Year, 12, 25, 0, 0, 1)
+
+clean()
+
+while True:
+    td = datetime.datetime.now()
+    delta = xd - td
+    print(delta)
+    time.sleep(1)
+    clean()
+print()
+
+#Failed tests:
+#    print(delta + "\r", end="")
+#while delta > datetime.datetime(0, 0, 0, 0, 0, 0, 0):
+
