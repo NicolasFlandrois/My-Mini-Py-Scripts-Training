e2e8965af0adbfb46560449e6e5a448e0601f591
NicolasFlandrois <n.flandrois9github@gmail.com>
2018-12-20 11:42:53 +0100

    Cleanning up line of codes, testing around. remaining issue = Exccept ErrorValue doesn't work

--- guess-the-number.py
+++ guess-the-number.py
@@ -7,6 +7,7 @@ import random
 import os
 import platform
 import time
+import sys
 
 def clean():
 	if platform.system() == "Windows":
@@ -16,7 +17,7 @@ def clean():
 
 print("Welcome to 'Guess the Number' game!")
 time.sleep(1)
-print("You have to guess an integer number, between 1 and 100, within 7 guess.")
+print("You have to guess a randomly defined whole number, between 1 and 100, within 7 guesses.")
 time.sleep(3)
 
 clean()
@@ -39,48 +40,56 @@ while True:
         trials -= 1
         y = int(input("Guess the number? "))
 
-#        if except ValueError:
-#            return ("Oops!  That was no valid number.  Try again...")
-#            pass
-#        elif:  
-#            y in r
-#                if False
-#                    return("Oops!  That was no valid number. Reminder choose a number between 1 and 100. Try again...")
-#                else:
-#                    continue
-#        finally:
+#Verification if input number is valid
+#        except:
+#            print("Oops! ",sys.exc_info()[0]," occured. That was no valid number.  Try again...")
 #            pass
 
+        if y not in r:
+            print("Oops!  That was no valid number. Reminder choose a number between 1 and 100. Try again...")
+#        elif type(y)==float:                                                                        #cette condition est couvert par exception ErrorValue, car on a indiqué que int(y) 
+#            print("Oops!  That was no valid number. Reminder choose a whole number. Try again...")
+
+#Test if player win or loose
         if y > x:
             print("You guessed too high!")
             trial()
-            time.sleep(3)
+            time.sleep(2)
             clean()
         elif y < x:
             print("You guessed too low!")
             trial()
-            time.sleep(3)
+            time.sleep(2)
             clean()
         elif y == x:
             print("You won!!!")
             win += 1
             time.sleep(1)
             score()
-            time.sleep(5)
+            time.sleep(3)
             clean()
             break
 
+#If after 7 guesses player didn't guess correctly, let's inform him he lost!
     if y != x:
         print("You lost!")
         time.sleep(1)
         score()
     else:
         continue
+
+#Start a new game (continue infinite While True loop) or Quit loop?
+    time.sleep(1)
+    answer = input("Do you want to play again? Y/N ").strip().lower()
+    if answer in ("no","n","q","quit"):
+        time.sleep(1)
+        clean()
+        print("Good bye! Thank you for playing.")
+        break
     
     time.sleep(5)
     clean()
 
 #Still to do:
-#Offer a choice to start a new game or quit
-#verify if input is: Interger + a positive number + within range [1;100]
+#verify if input is: a positive & whole number & within range [1;100]
 #use error value >> if ErrorValue: print("we said a number, WTF!") then continue or pass
