fe3395e0dcd10fe9c245a81f34c6ea76f150294c
NicolasFlandrois <n.flandrois9github@gmail.com>
2018-12-25 00:18:06 +0100

    updates & Optimisation

--- guess-the-number.py
+++ guess-the-number.py
@@ -11,51 +11,56 @@ import time
 import sys
 
 def clean():
-	if platform.system() == "Windows":
-		os.system("cls")
-	elif platform.system() == "Linux":
-		os.system("clear")
+    """.Le but de cette fonction est de vider la console, de façon auto déterminer sous Windows ou Linux."""
+    if platform.system() == "Windows":
+        os.system("cls")
+    elif platform.system() == "Linux":
+        os.system("clear")
 
 print("Welcome to 'Guess the Number' game!")
 time.sleep(1)
 print("""You have to guess a randomly defined whole number, 
 between 1 and 100, 
 within 7 guesses.""")
-time.sleep(5)
+print("(press any key to continue)")
+input()
 
 clean()
 
-loop = 0
-win = 0
+loop = None
+win = None
 def score():
+    """Le but de cette fonction est d'imprimer un score."""
     print('Your score is ' + str(win) + '/' + str(loop))
 
 def dubai():
+    """Le but de cette fonction est d'imprimer le nombre d'éssaie réstant, pendant 5 seconds, puis de vider la console."""
     print('remaining ' + str(trials) + ' trials.')
-    time.sleep(3)
+#    time.sleep(5)
+    print("(press any key to continue)")
+    input()
     clean()
 #named it "dubai", because why not! came short of a code name for this function.
 
 while True:
-    trials=int(7)
-    x = random.randrange(1,101,2)
-    loop += 1
-    r=range(1,101)
+    trials = int(7)
+    x = random.randrange(1, 101, 2)
+    loop+=1
 
     for i in range(7):
-        trials -= 1
+        trials-=1
 
         try:
             y = int(input("Guess the number? "))
 
 #Verification if input number is valid
         except:
-            print("Oops! ",sys.exc_info()[0]," occured. That was no valid number.  Try again...")
-            time.sleep(1)
+            print("Oops! ValueError occured. That was no valid number.  Try again...")
+            time.sleep(10)
             dubai()
             continue
 
-        if y not in r:
+        if y not in range(1,101):
             print("""Oops!  That was no valid number. 
 REMINDER: choose a number between 1 and 100. 
 Try again...""")
@@ -67,28 +72,29 @@ Try again...""")
         elif y < x:
             print("You guessed too low!")
             dubai()
-        elif y == x:
+        else:
             print("You won!!!")
-            win += 1
-            time.sleep(1)
+            win+=1
+            time.sleep(5)
             score()
-            time.sleep(2)
+            time.sleep(10)
             break
 
 #If after 7 guesses player didn't guess correctly, let's inform him he lost! Or if he won and break the loop, let's continue.
     if y != x:
         print("You lost!")
-        time.sleep(1)
+    print("(press any key to continue)")
+        input()
         score()
 
 #Start a new game (continue infinite While True loop) or Quit loop?
     time.sleep(1)
     answer = input("Do you want to play again? Y/N ").strip().lower()
     if answer in ("no","n","q","quit"):
-        time.sleep(1)
+        time.sleep(10)
         clean()
         print("Good bye! Thank you for playing.")
         break
     
-    time.sleep(1)
+    time.sleep(10)
     clean()
