02d8ed826c90cc28edd578be84c674788eefdf62
NicolasFlandrois <n.flandrois9github@gmail.com>
2018-12-27 11:37:49 +0100

    update description new ideas on DrinkDispenser project

--- drink_dispenser/drinkmachine.py
+++ drink_dispenser/drinkmachine.py
@@ -5,7 +5,7 @@
 
 # Description: 
 # We consider a drink dispenser, prices are integer, and the machine keeps change only in coins of 2€, 5€, 10€, in a finit amount. 
-# When the user (player) chooses a drink, the machine asks the player to pay the drink's price. The player can input any amount of money, >0€ & int().
+# When the user (player) chooses a drink, the machine asks the player to pay the drink's price. The player can input any amount of money, >0€ & int() & >drink_price_€.
 # Then the machine will determine the change to return to the player, within its account of finit amount of coins of 2€, 5€, and 10€.
 # It will first give change for biggest coin value, within available balance account for this coin value.
 # If it cannot, then it will give change for Second biggest value, within available balance account for this coin value.
@@ -24,3 +24,12 @@
 #   - At begining of launching script, ask what drinks and prices has to be set. Inclued them in a dictionary.
 #   - At begining of launching script, ask how many coins are in the machine, for each coin value... the initial "fond de caisse".
 #   - Give the balace Account, when interupting any loop with [ except keyInterrupt: ] command.
+#   - Establish an email alert. Down to a certain level of change coins amount(nb), send an email to dispenser Owner.
+#            - As soon as 1 coin ammount(nb) = 0 (or 1 coin)
+#            - Send email:
+#                    - To: owner
+#                    - Object: "No more change, please refill me"
+#                    - Gives Machine ID number
+#                    - Which coin value raised the alert?
+#                    - Gives a summary of Balance account
+#    - Make a balance account for number of drinks, which at every sale, it substract 1. If = 0 then print message "Out of this drink. Victim of its success."
