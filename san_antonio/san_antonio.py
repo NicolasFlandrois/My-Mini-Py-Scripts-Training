9f24f36a513a4c21277c78de62840c3da7ac4e92
NicolasFlandrois <n.flandrois9github@gmail.com>
2018-12-28 01:22:00 +0100

    Continue to work on San Antonio Quotes, Following the class exercices

--- san_antonio/san_antonio.py
+++ san_antonio/san_antonio.py
@@ -1,16 +1,31 @@
 # -*- coding: utf8 -*-
 
 quotes = [
-    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
-    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
+	"Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
+	"On doit pouvoir choisir entre s'écouter parler et se faire entendre."
 ]
 
 characters = [
-    "alvin et les Chipmunks",
-    "Babar",
-    "betty boop",
-    "calimero",
-    "casper",
-    "le chat potté",
-    "Kirikou"
+	"alvin et les Chipmunks",
+	"Babar",
+	"betty boop",
+	"calimero",
+	"casper",
+	"le chat potté",
+	"Kirikou"
 ]
+
+def get_random_quote(my_list):
+	"""This fonction show a random quote from a list."""
+#	import random
+#	n = random.randrange(0, 2, 2)
+	item = my_list[0]
+	return item
+user_answer = "A"
+while user_answer != "B":
+	user_answer = input("Tappez 'Entrer' pour executer le programme, et 'B' pour quitter le programme. ")
+	print(get_random_quote(quotes))
+
+for character in characters:
+	n_character = character.capitalize()
+	print(n_character)
