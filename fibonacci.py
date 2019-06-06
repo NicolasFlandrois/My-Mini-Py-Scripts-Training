175577c74321aab3e3bd0aa276427e697c8479fe
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-06 21:29:04 +0200

    Tribonacci
    Creating the script for a Tribonacci sequence. A Fibonacci variation adding together the 3 previous numbers of the sequence to compute n

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 #Date : Mon 28 Jan 2019 03:24:59 PM CET 
 #Author: Nicolas Flandrois
+#Description: This short script will compute the Fibonacci sequence.
+# n  = (n-2) + (n-1)
+#1, 1, 2, 3, 5, 8, 13, 21, etc
 
 def fib(n:int):
-	"""This generator yield each iteration of the fibonacci sequence, for iteration of a range n number."""
+	"""This generator yield each iteration of the fibonacci sequence, 
+	for iteration of a range n number."""
 	a, b = 0, 1
 	for i in range(n):
 		yield a
@@ -11,6 +15,7 @@ def fib(n:int):
 		
 n = int(input("To what number of recurence do you want your fibonacci list? "))
 fibonacci = [f for f in fib(n)]
-#Comprehension list applying the fibonacci generator, and holding the result in a list, sequences.
+#Comprehension list applying the fibonacci generator, and holding the result in 
+#a list, sequences.
 
 print(fibonacci)
---
+++ tribonacci.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#Date : Thu 06 Jun 2019 21:24:59 PM CET 
+#Author: Nicolas Flandrois
+#Description:This short script will compute the Tribonacci sequence.
+#The Tribonacci sequence is a variante of Fibonacci, where a number n in the 
+#sequence is equal to the sum of the 3 previous n number in the sequence.
+# n  = (n-3) + (n-2) + (n-1)
+#1, 1, 1, 3, 5, 9, 17, etc.
+
+def trib(n:int):
+	"""This generator yield each iteration of the tribonacci sequence, 
+	for iteration of a range n number."""
+	a, b, c = 1, 1, 1
+	for i in range(n):
+		yield a
+		a, b, c = b, c, a+b+c
+		
+n = int(input("To what number of recurence do you want your fibonacci list? "))
+tribonacci = [f for f in trib(n)]
+#Comprehension list applying the tribonacci generator, and holding the result in 
+#a list, sequences.
+
+print(tribonacci)
