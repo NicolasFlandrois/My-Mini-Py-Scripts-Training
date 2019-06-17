020c875c1d8d4554912c0caccdbd3014e877d68d
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-17 16:47:37 +0200

    Basic stackplot display,  over time

--- Matplotlib Practice/matplot4-StackPlots.py
+++ Matplotlib Practice/matplot4-StackPlots.py
@@ -1,4 +1,4 @@
- #MATPLOTLIB Pie Chart Practice
+ #MATPLOTLIB Stack Plots Practice
 
 from matplotlib import pyplot as plt
 
@@ -6,12 +6,16 @@ plt.style.use('fivethirtyeight')
 
 minutes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-player1 = [8, 6, 5, 5, 4, 2, 1, 1, 0]
-player2 = [0, 1, 2, 2, 2, 4, 4, 4, 4]
-player3 = [0, 1, 1, 1, 2, 2, 3, 3, 4]
+player1 = [1, 2, 3, 3, 4, 4, 4, 4, 5]
+player2 = [1, 1, 1, 1, 2, 2, 2, 3, 4]
+player3 = [1, 1, 1, 2, 2, 2, 3, 3, 3]
 
-plt.pie([1, 1, 1], labels=["Player 1", "Player 2", "Player 3"])
+# player1 = [8, 6, 5, 5, 4, 2, 1, 1, 0]
+# player2 = [0, 1, 2, 2, 2, 4, 4, 4, 4]
+# player3 = [0, 1, 1, 1, 2, 2, 3, 3, 4]
+
+plt.stackplot(minutes, player1, player2, player3)
 
 plt.title('Stack Plot Practice')
 plt.tight_layout()
-plt.savefig('matplot3-piechart.png')
+plt.savefig('matplot4-Stackplot.png')
