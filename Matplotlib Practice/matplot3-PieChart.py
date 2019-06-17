cc485878c08d5338e47a3cc578a0dce881924eb9
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-17 16:05:13 +0200

    Basic Pie chart function and graph

--- Matplotlib Practice/matplot3-PieChart.py
+++ Matplotlib Practice/matplot3-PieChart.py
@@ -5,6 +5,16 @@ from matplotlib import pyplot as plt
 
 plt.xkcd()
 
+slices = [60, 40]
+
+plt.pie(slices)
+
 plt.title('Awsome Pie Chart')
 plt.tight_layout()
 plt.savefig('matplot3-piechart1.png')
+
+# Colors:
+# Blue = #008fd5
+# Red = #fc4f30
+# Yellow = #e5ae37
+# 
