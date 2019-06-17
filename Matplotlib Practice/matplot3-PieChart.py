78eb9b1b19a4c7a851f10129aca4cf2748a10173
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-17 16:15:07 +0200

    customising colours with Hexadecimal colour pallet

--- Matplotlib Practice/matplot3-PieChart.py
+++ Matplotlib Practice/matplot3-PieChart.py
@@ -7,9 +7,10 @@ plt.xkcd()
 
 slices = [120, 80, 30, 20]
 labels = ['Not-sixty', 'Not-fourty', 'Extra1', 'Extra2']
-colors = ['blue', 'red', 'yellow', 'green']
+colors = ['#008fd5', '#fc4f30', '#e5ae37', '#6d904f']
 
-plt.pie(slices, labels=labels, colors=colors, wedgeprops={'edgecolor':'black'})
+plt.pie(slices, labels=labels, colors=colors, 
+	wedgeprops={'edgecolor':'black'})
 
 plt.title('Awsome Pie Chart')
 plt.tight_layout()
@@ -19,4 +20,4 @@ plt.savefig('matplot3-piechart1.png')
 # Blue = #008fd5
 # Red = #fc4f30
 # Yellow = #e5ae37
-# Green = 6d904f
+# Green = #6d904f
