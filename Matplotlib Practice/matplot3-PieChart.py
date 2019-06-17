c48ce1893e008fb6e6bbe9b7da67cd6276309869
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-17 16:21:48 +0200

    Too many data... What it looks like

--- Matplotlib Practice/matplot3-PieChart.py
+++ Matplotlib Practice/matplot3-PieChart.py
@@ -2,17 +2,21 @@
 
 from matplotlib import pyplot as plt
 
+plt.style.use('fivethirtyeight')
 
-plt.xkcd()
+# Language Popularity
+slices = [59219, 55466, 47544, 36443, 35917, 31991, 27097, 23030, 20524,
+18523, 18017, 7920, 7331, 7201, 5833]
+labels = ['JavaScript', 'HTML/CSS', 'SQL', 'Python', 'Java',
+'Bash/Shell/PowerShell', 'C#', 'PHP', 'C++', 'TypeScript', 'C', 'Other(s):',
+'Ruby', 'Go', 'Assembly']
 
-slices = [120, 80, 30, 20]
-labels = ['Not-sixty', 'Not-fourty', 'Extra1', 'Extra2']
-colors = ['#008fd5', '#fc4f30', '#e5ae37', '#6d904f']
+# Too many data, for a usable Pie Chart, that actually means something...
+# Use Pie Charts with max 5 data items, for more visual effects.
 
-plt.pie(slices, labels=labels, colors=colors, 
-	wedgeprops={'edgecolor':'black'})
+plt.pie(slices, labels=labels, wedgeprops={'edgecolor':'black'})
 
-plt.title('Awsome Pie Chart')
+plt.title('Programming language popularity')
 plt.tight_layout()
 plt.savefig('matplot3-piechart1.png')
 
