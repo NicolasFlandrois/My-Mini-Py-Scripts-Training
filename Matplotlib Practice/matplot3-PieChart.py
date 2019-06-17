ea99fba220d22448319eca4c5d88ce3431f0c4ea
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-17 16:27:55 +0200

    How to Explode (detach) a specific part
    emphasise a specific Item from the chart

--- Matplotlib Practice/matplot3-PieChart.py
+++ Matplotlib Practice/matplot3-PieChart.py
@@ -5,20 +5,17 @@ from matplotlib import pyplot as plt
 plt.style.use('fivethirtyeight')
 
 # Language Popularity
-slices = [59219, 55466, 47544, 36443, 35917, 31991, 27097, 23030, 20524,
-18523, 18017, 7920, 7331, 7201, 5833]
-labels = ['JavaScript', 'HTML/CSS', 'SQL', 'Python', 'Java',
-'Bash/Shell/PowerShell', 'C#', 'PHP', 'C++', 'TypeScript', 'C', 'Other(s):',
-'Ruby', 'Go', 'Assembly']
+slices = [59219, 55466, 47544, 36443, 35917]
+labels = ['JavaScript', 'HTML/CSS', 'SQL', 'Python', 'Java']
+explode = [0, 0, 0, 0.1, 0] # Explode == Detach and expose a zone from chart.
 
-# Too many data, for a usable Pie Chart, that actually means something...
-# Use Pie Charts with max 5 data items, for more visual effects.
 
-plt.pie(slices, labels=labels, wedgeprops={'edgecolor':'black'})
+plt.pie(slices, labels=labels, explode=explode,
+	wedgeprops={'edgecolor':'black'})
 
 plt.title('Programming language popularity')
 plt.tight_layout()
-plt.savefig('matplot3-piechart1.png')
+plt.savefig('matplot3-piechart.png')
 
 # Colors:
 # Blue = #008fd5
