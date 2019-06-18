a8f70e5ba1d48ebc1d117de8f8e89315d52840ff
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-18 12:57:32 +0200

    Histogram from CSV

--- Matplotlib Practice/matplot6-Histograms.py
+++ Matplotlib Practice/matplot6-Histograms.py
@@ -5,15 +5,13 @@ from matplotlib import pyplot as plt
 
 plt.style.use('fivethirtyeight')
 
-ages = [18, 19, 21, 25, 26, 26, 30, 32, 38, 45, 55]
-bins = [20, 30, 40 , 50, 60]
+data = pd.read_csv('data3.csv')
+ids = data['Responder_id']
+ages = data['Age']
+
+bins = [10, 20, 30, 40 , 50, 60, 70, 80, 90, 100]
 
 plt.hist(ages, bins=bins, edgecolor='black')
-# Grouping the data in Bins, with a list (can also be done with an Integer)
-
-# data = pd.read_csv('data3.csv')
-# ids = data['Responder_id']
-# ages = data['Age']
 
 # median_age = 29
 # color = '#fc4f30'
