03e0ca61c900ba48f721bcd5146ba02cfc14a1ab
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-18 13:52:13 +0200

    Dynamic colour and colour legend

--- Matplotlib Practice/matplot7-ScatterPlot.py
+++ Matplotlib Practice/matplot7-ScatterPlot.py
@@ -12,7 +12,11 @@ view_count = data['view_count']
 likes = data['likes']
 ratio = data['ratio']
 
-plt.scatter(view_count, likes, edgecolor='black', linewidth=1, alpha=0.75)
+plt.scatter(view_count, likes, c=ratio, cmap='summer', edgecolor='black',
+			linewidth=1, alpha=0.75)
+
+cbar = plt.colorbar()
+cbar.set_label('Like/Dislike Ratio')
 
 plt.xscale('log')
 plt.yscale('log')
