be844ded5e706968ea359a2d1ac2bdf431d82993
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-18 12:59:52 +0200

    Make data more visible in large set of data, with Log repartition

--- Matplotlib Practice/matplot6-Histograms.py
+++ Matplotlib Practice/matplot6-Histograms.py
@@ -11,7 +11,7 @@ ages = data['Age']
 
 bins = [10, 20, 30, 40 , 50, 60, 70, 80, 90, 100]
 
-plt.hist(ages, bins=bins, edgecolor='black')
+plt.hist(ages, bins=bins, edgecolor='black', log=True)
 
 # median_age = 29
 # color = '#fc4f30'
