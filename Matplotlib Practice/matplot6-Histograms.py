078e4fb8ff9a83af41a746b95c6f2def069f7f62
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-18 12:43:55 +0200

    Grouping data bins 2 methods
    Grouping by integer
    Grouping custom with a list

--- Matplotlib Practice/matplot6-Histograms.py
+++ Matplotlib Practice/matplot6-Histograms.py
@@ -6,8 +6,10 @@ from matplotlib import pyplot as plt
 plt.style.use('fivethirtyeight')
 
 ages = [18, 19, 21, 25, 26, 26, 30, 32, 38, 45, 55]
+bins = [10, 20, 30, 40 , 50, 60]
 
-plt.hist(ages, bins=5)
+plt.hist(ages, bins=bins, edgecolor='black')
+# Grouping the data in Bins, with a list (can also be done with an Integer)
 
 # data = pd.read_csv('data3.csv')
 # ids = data['Responder_id']
