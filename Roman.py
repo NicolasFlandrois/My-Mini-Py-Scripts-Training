042f148f50dd830b666bf74513ac8c98c0ae8590
NicolasFlandrois <n.flandrois9github@gmail.com>
2022-05-18 22:41:56 +0200

    Updated Roman Numerals Translator
    That's one way of doing it. But Isn't the most mathematical approach, nor the most optimized.

--- Roman.py
+++ Roman.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # Author: Nicolas Flandrois
 # Date: Fri 24 July 2020 11:23:44
-# Last Modified : Wed 18 May 2022 16:57:49 CEST
+# Last Modified : Wed 18 May 2022 22:38:08 CEST
 
 
 def roman_convert(dec_num: int) -> str:
@@ -44,7 +44,83 @@ def roman_convert(dec_num: int) -> str:
         return ''.join(rom_tmp_lst)
 
 
+def roman_translate(rom_num: str) -> int:
+    '''
+    Translating a Roman numeral into modern decimal (positive integer) number.
+    The submitted roman numeral will be between 'I'(1) and 'MMMCMXCIX'(3999).
+
+    It must follow the common rules of roman numeral system. Which includes no
+    suite of same letters more than 3 times in a raw.
+
+    For which [900, 400, 90, 40, 9, 4] would have special treatment,
+    and be converted as [CM, CD, XC, XL, IX, IV]. That is also why we are
+    limited to a max of 3999, because 4000 would be "MMMM" 4 M in a raw,
+    and won't be legitimate.
+    And notably, Zero doesn't exists.
+
+    Argument
+    -----------
+    rom_num (str): Roman Numeral String Positive Integer from 1 to 3999.
+                   Usually Roman Numerals are Upper Cases, but here we accept
+                   Lower Cases as well, it will be converted into upper cases.
+
+    Return
+    --------
+    int: If the process succeed, the translated integer will be returned,
+         comprised in a range between 1, and 3999.
+    str: In any errors a Message will be delivered.
+
+    A good website reference, to explain how Roman Numerals works:
+    https://all-about-roman-numerals.com/
+
+    __Developper's Notes__:
+        I am well aware this is not the most mathematical, nor the most
+        optimized process to translate a Roman Numeral into modern numbers.
+        Recreating the dictionary, and storing the entire 3999 numbers in
+        memory, can have LOTS of disadvantages (For Massive Implementations).
+        But considering we operate in a finite range of allowed numbers (rules
+        are rules), and that this is the easiest way to implement it.
+        This is still a work in process for more mathematical/optimized
+        wizardry.
+        However, with today's technology (CPU speed, and RAM capacities), and
+        considering that statistically this function won't be use at massive
+        scale or frequency... I believe it's still a valid algorithm.
+    '''
+    rom_dict = {roman_convert(n): n for n in range(1, 4000)}
+    if type(rom_num) is not str or rom_num.upper() not in rom_dict:
+        return f"Out of range [1, 3999], or this is not a Roman Numeral,\
+or is not a string:\t{rom_num}\t{type(rom_num)}"
+    else:
+        return rom_dict[rom_num.upper()]
+
+
+# Testing & Illustration (proof of concept)
 # A few Exemples:
+test_rom_num = ["TEST", 123, "123", "MMXXII", 'mmxxii', 'III', 'VIII', 'XII',
+                "XXXVII", "LXXXII", "CCCLIII", "XVIII", "LXI", "DCXXXII",
+                "MMVI", "MMMM", "MMMCMXCIX"]
+for m in test_rom_num:
+    print(m, ':\t', roman_translate(m))
+
+# Expected output
+# TEST :   out of range - this text is not a Roman Numeral
+# 123 :    out of range - This is an INT, not a str
+# 123 :    out of range - This STR is composed of digits, not RomanNumerals
+# MMXXII :     2022
+# mmxxii :     2022
+# III :    3
+# VIII :   8
+# XII :    12
+# XXXVII :     37
+# LXXXII :     82
+# CCCLIII :    353
+# XVIII :  18
+# LXI :    61
+# DCXXXII :    632
+# MMVI :   2006
+# MMMM :   out of range -> 'MMMM' = 4 x 1000 = 4000 out of range 1 to 3999
+# MMMCMXCIX :  3999
+
 for n in range(20):
     print(n, ':\t', roman_convert(n))
 
