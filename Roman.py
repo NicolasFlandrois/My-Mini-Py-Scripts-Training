ba70675b79519e8324a8e4fea640dc4947eabd93
NicolasFlandrois <n.flandrois9github@gmail.com>
2022-05-18 17:26:08 +0200

    Updated Roman Numerals Converter
    The previous version was found on internet and was too C++ like.
    I created a simpler and more Python version, that works well. Accepting base10 Positive Integer between 1 and 3999... because Ancients' Roman Numerals had rules to follow...

--- Roman.py
+++ Roman.py
@@ -1,108 +1,107 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Author: Nicolas Flandrois
-# Date:   Fri 24 July 2020 11:23:44
-# Last Modified time: Fri 31 July 2020 17:07:49 
-
-# Description:
-
-# coding = zip(
-#     [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
-#     ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-# )
-
-# # print(coding)
-# # for d, r in coding:
-# #     print('d', d)
-# #     print('r', r)
+# Date: Fri 24 July 2020 11:23:44
+# Last Modified : Wed 18 May 2022 16:57:49 CEST
 
 
-# def decToRoman(num: int):
-#     if num <= 0 or num >= 4000:
-#         raise ValueError('Input should be an integer between 1 and 3999')
-#     result = []
-#     for d, r in coding:
-#         while num >= d:
-#             result.append(r)
-#             num -= d
-#     return ''.join(result)
+def roman_convert(dec_num: int) -> str:
+    '''
+    Convert a decimal number (Positive Integer) between 1 and 3999
+    to a Roman numeral equivalent.
+
+    It must follow the common rules of roman numeral system. Which includes no
+    suite of same letters more than 3 times in a raw.
+
+    For which [900, 400, 90, 40, 9, 4] would have special treatment,
+    and be converted as [CM, CD, XC, XL, IX, IV]. That is also why we are
+    limited to a max of 3999, because 4000 would be "MMMM" 4 M in a raw,
+    and won't be legitimate.
+    And notably, Zero doesn't exists.
+
+    Argument
+    -----------
+    dec_num (int): Positive Integer from 1 to 3999
+
+    Return
+    --------
+    str: Either a Message error if out of range,
+         or the Roman Converted numeral.
+
+    A good website reference, to explain how Roman Numerals works:
+    https://all-about-roman-numerals.com/
+    '''
+    if dec_num not in range(1, 4000):
+        return "Out of range - Should be between 1 and 3999."
+    else:
+        romdict = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100,
+                   'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5,
+                   'IV': 4, 'I': 1}
+        rom_tmp_lst = []
+        for key, val in romdict.items():
+            rom_tmp_lst.append((dec_num // val)*key)
+            dec_num = dec_num % val
+        return ''.join(rom_tmp_lst)
 
 
-# # for i in range(1, 4000):
-#     # print(i, decToRoman(i))
-# # for i in range(1, 10):
-# #     print(decToRoman(i))
-# # print(decToRoman(0))
-# print(decToRoman(1))
-# print(decToRoman(2))
-# print(decToRoman(3))
-# print(decToRoman(4))
-# print(decToRoman(5))
-# print(decToRoman(6))
-# print(decToRoman(7))
-# print(decToRoman(8))
-# print(decToRoman(9))
-# print(decToRoman(10))
-# print(decToRoman(11))
-# print(decToRoman(12))
-# print(decToRoman(13))
-# # print(decToRoman('A'))
-###
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
-                 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
-        i = 0
-        num = 0
-        while i < len(s):
-            if i + 1 < len(s) and s[i:i + 2] in roman:
-                num += roman[s[i:i + 2]]
-                i += 2
-            else:
-                # print(i)
-                num += roman[s[i]]
-                i += 1
-        return num
+# A few Exemples:
+for n in range(20):
+    print(n, ':\t', roman_convert(n))
 
+# This should look like:
+# 0 :  Out of range - Should be between 1 and 3999.
+# 1 :  I
+# 2 :  II
+# 3 :  III
+# 4 :  IV
+# 5 :  V
+# 6 :  VI
+# 7 :  VII
+# 8 :  VIII
+# 9 :  IX
+# 10 :     X
+# 11 :     XI
+# 12 :     XII
+# 13 :     XIII
+# 14 :     XIV
+# 15 :     XV
+# 16 :     XVI
+# 17 :     XVII
+# 18 :     XVIII
+# 19 :     XIX
 
-ob1 = Solution()
-print(ob1.romanToInt("III"))
-print(ob1.romanToInt("CDXLIII"))
-###
-# from collections import OrderedDict
+test_list = [0, -1, 20, 40, 44, 50, 55, 77, 99, 100, 490, 500, 600, 649, 899,
+             900, 999, 1000, 1754, 1998, 1999, 2022, 2999, 3000, 3549, 3999,
+             4000, 5000]
+for n in test_list:
+    print(n, ':\t', roman_convert(n))
 
-def write_roman(num):
-
-    # roman = OrderedDict()
-    roman = {}
-    roman[1000] = "M"
-    roman[900] = "CM"
-    roman[500] = "D"
-    roman[400] = "CD"
-    roman[100] = "C"
-    roman[90] = "XC"
-    roman[50] = "L"
-    roman[40] = "XL"
-    roman[10] = "X"
-    roman[9] = "IX"
-    roman[5] = "V"
-    roman[4] = "IV"
-    roman[1] = "I"
-
-    def roman_num(num):
-        for r in roman.keys():
-            x, y = divmod(num, r)
-            yield roman[r] * x
-            num -= (r * x)
-            if num <= 0:
-                break
-
-    return "".join([a for a in roman_num(num)])
-
-
-for i in range(1, 10):
-    print(write_roman(i))
+# This should look like this:
+# 0 :  Out of range - Should be between 1 and 3999.
+# -1 :     Out of range - Should be between 1 and 3999.
+# 20 :     XX
+# 40 :     XL
+# 44 :     XLIV
+# 50 :     L
+# 55 :     LV
+# 77 :     LXXVII
+# 99 :     XCIX
+# 100 :    C
+# 490 :    CDXC
+# 500 :    D
+# 600 :    DC
+# 649 :    DCXLIX
+# 899 :    DCCCXCIX
+# 900 :    CM
+# 999 :    CMXCIX
+# 1000 :   M
+# 1754 :   MDCCLIV
+# 1998 :   MCMXCVIII
+# 1999 :   MCMXCIX
+# 2022 :   MMXXII
+# 2999 :   MMCMXCIX
+# 3000 :   MMM
+# 3549 :   MMMDXLIX
+# 3999 :   MMMCMXCIX
+# 4000 :   Out of range - Should be between 1 and 3999.
+# 5000 :   Out of range - Should be between 1 and 3999.
